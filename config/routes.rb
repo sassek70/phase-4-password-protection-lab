68bb3e126a9ee9cf61dadc7812a4a4e6cd5c800a
Kevin Sasse <sassek70@gmail.com>
2022-12-06 10:32:14 -0500

    finished lab

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :users
+
+  post "/login", to: "sessions#create"
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
